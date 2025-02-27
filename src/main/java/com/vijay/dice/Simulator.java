5aa9995da11e20c21a7fe094c06a57c78a7919d8
vijaysm-git <reachvijaysm@outlook.com>
2025-02-27 16:38:36 +0530

    Initial commit

---
+++ src/main/java/com/vijay/dice/Simulator.java
@@ -0,0 +1,12 @@
+package com.vijay.dice;
+
+import java.util.Random;
+
+
+public class Simulator {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.print(rand.nextInt(5));
+
+    }
+}
